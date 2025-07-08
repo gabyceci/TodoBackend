8a64d5df2cec169c04c332f3e1a17da256c7a217
gabyceci <20190019@gmail.com>
2025-07-07 20:10:18 -0600

    fix

--- src/controllers/todocontroller.js
+++ src/controllers/todocontroller.js
@@ -1,10 +1,10 @@
 const todocontroller = {};
 
-import Todos from "../models/todo.js"
+import todoSchema from "../models/Todo.js"
 
 todocontroller.getAll = async (req, res) => {
   try {
-    const todos = await Todos.find();
+    const todoSchema = await Todos.find();
     res.status(200).json(todos);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving todos" });
@@ -14,7 +14,7 @@ todocontroller.getAll = async (req, res) => {
 todocontroller.create = async (req, res) => {
   const { title, description, completed } = req.body;
   try {
-    const newTodo = new Todos({ title, description, completed });
+    const newTodo = new todoSchema({ title, description, completed });
     await newTodo.save();
     res.status(201).json(newTodo);
   } catch (error) {
@@ -26,7 +26,7 @@ todocontroller.update = async (req, res) => {
   const { id } = req.params;
   const { title, description, completed } = req.body;
   try {
-    const updatedTodo = await Todos.findByIdAndUpdate(
+    const updatedTodo = await todoSchema.findByIdAndUpdate(
       id,
         { title, description, completed },
         { new: true }
@@ -43,7 +43,7 @@ todocontroller.update = async (req, res) => {
 todocontroller.delete = async (req, res) => {
   const { id } = req.params;
   try {
-    const deletedTodo = await Todos.findByIdAndDelete(id);
+    const deletedTodo = await todoSchema.findByIdAndDelete(id);
     if (!deletedTodo) {
       return res.status(404).json({ message: "Todo not found" });
     }
@@ -56,7 +56,7 @@ todocontroller.delete = async (req, res) => {
 todocontroller.getById = async (req, res) => {
   const { id } = req.params;
     try {
-        const todo = await Todos.findById(id);
+        const todo = await todoSchema.findById(id);
         if (!todo) {
             return res.status(404).json({ message: "Todo not found" });
         }
